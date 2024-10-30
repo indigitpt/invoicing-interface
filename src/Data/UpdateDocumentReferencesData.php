d6f6f2c2b9bdaa48d165d214662a7529dd09bf0f
nunoxavier <insaneburn@gmail.com>
2024-10-30 16:58:47 +0000

    Standardise identifier naming

--- src/Data/UpdateDocumentReferencesData.php
+++ src/Data/UpdateDocumentReferencesData.php
@@ -9,6 +9,8 @@ class UpdateDocumentReferencesData extends Data
     public function __construct(
         public string $reference_website, // ex: payment.id
         public string $reference_financial_document, // related financial document identifier (uuid)
-        public ?DocumentMetaData $meta = null
+        public ?string $reference_provider = null, // moloni document identifier
+        public ?string $document_label = null, // moloni document label (ex: M/22)
+        public ?string $document_link = null // direct link for the pdf file
     ) {}
 }
